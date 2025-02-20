cb09283364edbbeca98efe883e19bd1cf647f273
VasylBiliak <bilyakvasya@ukr.net>
2025-02-20 14:35:07 -0500

    feat: replace main background image, optimize background styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,6 @@ import Header from "../components/navbar";
 import Footer from "./Footer";
 import BackToTop from "./backToBtn";
 import "../styles/global.css";
-// import Helmet from "./helmet";
 import BackgroundImage from "./backgroundImage";
 
 interface LayoutProps {
@@ -13,7 +12,6 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <div className="layout">
-            {/*<Helmet />*/}
             <BackgroundImage />
             <Header />
             <main className="content">{children}</main>
--- src/components/backgroundImage/BackgroundImage.tsx
+++ src/components/backgroundImage/BackgroundImage.tsx
@@ -1,46 +1,49 @@
 import React, { useEffect, useState } from "react";
-import MySVG from "./MySVG";
+import MySVG from "./MySVG2";
 import * as styles from "./backgroundImage.module.css";
 import OwlImage from "./OwlImage";
 import TitImage from "./TitImage";
+import {StaticImage} from "gatsby-plugin-image";
 
 const BackgroundImage: React.FC = () => {
-    const [theme, setTheme] = useState<string>("");
+    const [theme, setTheme] = useState<string>("light");
     const [colorSvg, setColorSvg] = useState<string>("");
     const [strokeWidth, setStrokeWidth] = useState<string>("0.5");
 
     useEffect(() => {
-        if (typeof window !== "undefined") {
-            const updateTheme = () => {
-                const currentTheme = localStorage.getItem("theme") || document.documentElement.getAttribute("data-theme") || "";
-                setTheme(currentTheme);
-                setStrokeWidth(currentTheme === "dark" ? "1.5" : "0.5");
+        if (typeof window === "undefined") return; // Уникнення SSR помилок
 
-                const color = getComputedStyle(document.documentElement)
-                    .getPropertyValue("--svg-color")
-                    .trim();
-                setColorSvg(color);
-            };
+        const updateTheme = () => {
+            const currentTheme = localStorage.getItem("theme") || document.documentElement.getAttribute("data-theme") || "light";
+            setTheme(currentTheme);
+            setStrokeWidth(currentTheme === "dark" ? "1.5" : "0.5");
 
-            updateTheme();
+            const color = getComputedStyle(document.documentElement)
+                .getPropertyValue("--svg-color")
+                .trim();
+            setColorSvg(color);
+        };
 
-            const observer = new MutationObserver(updateTheme);
-            observer.observe(document.documentElement, { attributes: true, attributeFilter: ["data-theme"] });
+        updateTheme();
 
-            return () => observer.disconnect();
-        }
+        const observer = new MutationObserver(updateTheme);
+        observer.observe(document.documentElement, { attributes: true, attributeFilter: ["data-theme"] });
+
+        return () => observer.disconnect();
     }, []);
 
     return (
         <div className={styles.background_container}>
-            {theme === "dark" ? <OwlImage /> : theme === "light" ? <TitImage /> : null}
-            <div className={styles.background_wrapper}>
-                <div className={styles.mirrored_svg}>
-                    <MySVG stroke={colorSvg} width="100%" height="100%" strokeWidth={strokeWidth} quality={100} />
-                </div>
+            <div className={styles.wrapper_first_image}>
+                {theme === "light" ? <TitImage /> : <OwlImage />}
+            </div>
                 <div className={styles.background_image}>
-                    <MySVG stroke={colorSvg} width="100%" height="100%" strokeWidth={strokeWidth} quality={10} />
-                </div>
+                    <MySVG stroke={colorSvg}
+                           width="100%"
+                           height="100%"
+                           strokeWidth={strokeWidth}
+                           quality={100}
+                    />
             </div>
         </div>
     );
